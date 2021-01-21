ddf9e706e4d40475e3264161e5841c9f8b39e576
Tanay Maheshwari <tanaymodani18@gmail.com>
2021-01-21 17:32:04 +0530

    lecture8: added template tags

--- DjangoCrashCourse/proj/accounts/views.py
+++ DjangoCrashCourse/proj/accounts/views.py
@@ -1,12 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import *
 # Create your views here.
 
 def home(request):
-    return render(request,'accounts/home.html')
+    customers = Customer.objects.all()
+    orders = Order.objects.all()
+    total_orders = orders.count()
+    pending = orders.filter(status='Pending').count()
+    delivered = orders.filter(status='Delivered').count()
+    context = {
+        'customers': customers,
+        'orders': orders,
+        'pending': pending,
+        'delivered': delivered,
+        'total_orders': total_orders
+    }
+    return render(request,'accounts/home.html', context)
 
 def products(request):
-    return render(request,'accounts/products.html')
+    products = Product.objects.all()
+    return render(request,'accounts/products.html', {'products': products})
 
 def customer(request):
     return render(request,'accounts/customer.html')
