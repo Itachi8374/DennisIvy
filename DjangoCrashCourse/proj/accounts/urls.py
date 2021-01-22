86455e3d01c70171ca4d684fdbcf88c134c1ffc6
Tanay Maheshwari <tanaymodani18@gmail.com>
2021-01-22 20:08:48 +0530

    lecture14: user authentication and registration

--- DjangoCrashCourse/proj/accounts/urls.py
+++ DjangoCrashCourse/proj/accounts/urls.py
@@ -7,5 +7,9 @@ urlpatterns = [
     path('customer/<slug:slug>', views.customer, name="customer"),
     path('create_order/<slug:slug>', views.CreateOrder, name="create_order"),
     path('update_order/<slug:slug>', views.UpdateOrder, name="update_order"),
-    path('delete_order/<slug:slug>', views.DeleteOrder, name="delete_order")
+    path('delete_order/<slug:slug>', views.DeleteOrder, name="delete_order"),
+    path('register/', views.registerPage, name="register"),
+    path('login/', views.loginPage, name="login"),
+    path('logout/', views.logoutUser, name="logout"),
+
 ]
--- DjangoCrashCourse/proj/accounts/views.py
+++ DjangoCrashCourse/proj/accounts/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 from django.http import HttpResponse
 from django.forms import inlineformset_factory
+
 from .models import *
-from .forms import OrderForm
+from .forms import OrderForm, CreateUserForm
+from .filters import OrderFilter
  
 # Create your views here.
 
+@login_required(login_url='login')
 def home(request):
     customers = Customer.objects.all()
     orders = Order.objects.all()
@@ -21,24 +28,69 @@ def home(request):
     }
     return render(request,'accounts/home.html', context)
 
+
+def registerPage(request):
+    form = CreateUserForm()
+    
+    if request.method=="POST":
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            user = form.cleaned_data.get('username')
+            messages.success(request, 'Account was created for '+user)
+            return redirect('login')
+    
+    context ={'form': form}
+    return render(request, 'accounts/register.html', context)
+
+
+def loginPage(request):
+
+    if request.method =="POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username = username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.info(request, 'Username or password is incorrect')
+    context ={}
+    return render(request, 'accounts/login.html', context)
+
+
+
+def logoutUser(request):
+    logout(request)
+    return redirect('login')
+
+
+@login_required(login_url='login')
 def products(request):
     products = Product.objects.all()
     return render(request,'accounts/products.html', {'products': products})
 
+
+@login_required(login_url='login')
 def customer(request, slug):
     customer = Customer.objects.get(id=slug)
     orders = customer.order_set.all()
     order_count = orders.count()
+    myFilter = OrderFilter(request.GET, queryset=orders)
+    orders = myFilter.qs
     context = {
         'orders': orders,
         'customer': customer,
-        'order_count': order_count
+        'order_count': order_count,
+        'myfilter': myFilter
     }
-    print(context)
+    
     return render(request,'accounts/customer.html', context)
 
+
+@login_required(login_url='login')
 def CreateOrder(request, slug):
-    OrderFormSet = inlineformset_factory(Customer, Order, fields =('product', 'status'), extra=10)
+    OrderFormSet = inlineformset_factory(Customer, Order, fields =('product', 'status', 'note'), extra=10)
     customer= Customer.objects.get(id=slug)
     formset = OrderFormSet(queryset=Order.objects.none(),instance = customer)
     context ={'formset': formset}
@@ -49,20 +101,26 @@ def CreateOrder(request, slug):
             return redirect('/')
     return render(request,'accounts/order_form.html', context)
 
+
+@login_required(login_url='login')
 def UpdateOrder(request, slug):
+    OrderFormSet = inlineformset_factory(Customer, Order, fields =('product', 'status', 'note'), extra=0)
     order = Order.objects.get(id=slug)
-    form = OrderForm(instance=order)
+    formset = OrderFormSet(instance = order.customer)
 
     if request.method=="POST":
-        form = OrderForm(request.POST, instance = order)
-        if form.is_valid:
-            form.save()
+        formset = OrderFormSet(request.POST, instance = order.customer)
+        if formset.is_valid:
+            formset.save()
             print(request.POST)
             return redirect('/')
-    context = {'form': form}
+
+    context = {'formset': formset}
 
     return render(request,'accounts/order_form.html', context)
 
+
+@login_required(login_url='login')
 def DeleteOrder(request, slug):
     order = Order.objects.get(id=slug)
     if request.method=="POST":
